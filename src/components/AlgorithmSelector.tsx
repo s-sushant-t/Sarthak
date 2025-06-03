a3b74118ae5553d4b1fb4a4995d485d5d03e585e
s-sushant-t <133092961+s-sushant-t@users.noreply.github.com>
2025-06-03 15:48:55 +0530

    Updated AlgorithmSelector.tsx

--- src/components/AlgorithmSelector.tsx
+++ src/components/AlgorithmSelector.tsx
@@ -146,7 +146,7 @@ const AlgorithmSelector: React.FC<AlgorithmSelectorProps> = ({ results, selected
         
         {results.custom && (
           <div className="relative pt-3">
-            <div className="absolute inset-0 flex items-center" aria-hidden="true">
+            <div className="absolute inset-0 flex items-center\" aria-hidden="true">
               <div className="w-full border-t border-gray-200"></div>
             </div>
             <div className="relative flex justify-center">
